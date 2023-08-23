82146e26f6a590767e0bf9427e8f5fe327bf8a9c
Михаил <misha13022008@gmail.com>
2023-08-23 23:15:46 +0300

    small clean up

--- Wist2MsilFrontend/WistVisitor.cs
+++ Wist2MsilFrontend/WistVisitor.cs
@@ -270,10 +270,8 @@ public sealed class WistVisitor : WistGrammarBaseVisitor<object?>
         _curFunc.Image.Dup();
         _saveResultLevel--;
 
-        for (var index = 1; index < context.expression().Length; index++)
-        {
+        for (var index = 1; index < context.expression().Length; index++) 
             Visit(context.expression(index));
-        }
 
         var argsCount = context.expression().Length;
         var fullName = WistFuncName.CreateFullName(context.IDENTIFIER().GetText(), argsCount, null);
