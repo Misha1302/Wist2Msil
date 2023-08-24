2422dede30506adc64b768c55d62e26c5e7127e9
Михаил <misha13022008@gmail.com>
2023-08-24 14:31:51 +0300

    dlls compilation paths changed

--- Wist2Msil/NumberParser.cs
+++ Wist2Msil/NumberParser.cs
@@ -6,6 +6,6 @@ public static class WistNumberParser
 {
     private static readonly CultureInfo _dotCulture = new("en") { NumberFormat = { NumberDecimalSeparator = "." } };
 
-    public static double ToDouble(this string s) => double.Parse(s, NumberStyles.Any, _dotCulture);
+    public static double ToDouble(this string s) => double.Parse(s.Replace("_", ""), NumberStyles.Any, _dotCulture);
     public static bool ToBool(this string s) => s.ToLower() is "true" or "yes";
 }
---
+++ WistTime/WistTime.cs
@@ -0,0 +1,11 @@
+﻿namespace WistTime;
+
+using Wist2MsilFrontend;
+using WistConst;
+
+[WistLibrary]
+public static class WistTime
+{
+    [WistFunction]
+    public static WistConst GetTimeInMs() => new(DateTimeOffset.Now.ToUnixTimeMilliseconds());
+}
